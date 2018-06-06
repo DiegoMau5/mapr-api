fb0b0edea1140b4b33f2deed76e4eed5e6b33bcd
DIego <diego.gblanco94@gmail.com>
2018-06-06 17:57:11 +0200

    third upload

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,4 +3,7 @@ module.exports = function(app){
   app.get('/', (req,res)=>{
     res.status(200).send("SERVIDOR ACITVO API");
   });
+  app.get('/hola', (req,res)=>{
+    res.status(200).send("hola, probando");
+  });
 }
