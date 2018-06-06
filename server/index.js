d19439f99255d8d4de43f99072133865c1af1522
DIego <diego.gblanco94@gmail.com>
2018-06-06 17:55:29 +0200

    second upload

--- server/index.js
+++ server/index.js
@@ -4,11 +4,18 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var server = require('http').Server(app);
+var morgan = require('morgan');
+
+//routes del SERVIDOR
+
+require('./routes/routes.js')(app);
+
+//middleware
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
 
 server.listen(process.env.PORT || 3000, function(){
   console.log('Servidor running...')
-});
-
-app.get('/', function(req, res){
-  res.status(200).send("SERVIDOR ACITVO API")
 });
