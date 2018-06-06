4ecdf8d792f653064f65330f169e8e8cad8ab568
DIego <diego.gblanco94@gmail.com>
2018-06-06 21:37:27 +0200

    database developed

--- server/bd/query.js
+++ server/bd/query.js
@@ -11,5 +11,40 @@ connection = mysql.createConnection({
 
 let queryModels = {}
 
+queryModels.getAreas = (callback) => {
+  if (connection) {
+    connection.query(
+      'SELECT * FROM areas ORDER BY id',
+      (err,rows)=>{
+        if(err){
+          throw err;
+        }
+        else {
+          callback(null, rows);
+        }
+      }
+    )
+  }
+};
+
+queryModels.getCamaras = (area , callback) =>{
+  if (connection) {
+    console.log(area);
+    var sqlExists = `
+      select name_area, name_calle, posx, posy from camaras
+      inner join calles on camaras.id_calle = calles.id
+      inner join areas on calles.id_area = areas.id
+      where areas.name_area = ${connection.escape(area)}
+    `;
+    connection.query(sqlExists, (err, rows)=>{
+      if (err) {
+        throw err;
+      }
+      else{
+        callback(null, rows)
+      }
+    });
+  }
+}
 
 module.exports = queryModels;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,4 +6,17 @@ module.exports = function(app){
   app.get('/hola', (req,res)=>{
     res.status(200).send("hola, probando");
   });
+  app.get('/areas', (req,res)=>{
+    Query.getAreas((err, data) => {
+      res.status(200).json(data);
+    });
+  });
+
+  app.get('/camaras/:area',(req, res) =>{
+    Query.getCamaras(req.params.area, (err, data)=>{
+      if (data) {
+        res.status(200).json(data)
+      }
+    });
+  });
 }
